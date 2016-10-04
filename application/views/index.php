1f15c68589e30aa098f3953e2335262e8f5dff37
yuyuyut <646614928@qq.com>
2016-10-04 13:54:45 +0800

    更改基础配置

---
+++ application/views/index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>首页</title>
+    <base href="<?php echo site_url();?>">
+    <script>
+        window.addEventListener("load",function(){
+            setTimeout(hideURLbar,0);
+        },false);
+        function hideURLbar(){
+            window.scrollTo(0,1);
+        }
+    </script>
+</head>
+<body>
+
+<img src="img/head.png" alt="">
+
+</body>
+</html>
